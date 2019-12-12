f3c9d2fc986fafb913084fea3ca754a8ae400606
Andres Ricardo Perez <riperezro@unal.edu.co>
2019-12-12 10:31:22 -0500

    Front end project creation

---
+++ code/se1-front/app/src/main/java/co/edu/unal/se1_app/presentation/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package co.edu.unal.se1_app.presentation.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import co.edu.unal.se1_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
